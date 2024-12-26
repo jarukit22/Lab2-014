adf85f53571dc0755de3c707012b5d235ad71b59
Jarukit22 <jarukit22646@gmail.com>
2024-12-26 16:56:22 +0700

    แก้ไขไฟล์ contact.php #2

--- contact.php
+++ contact.php
@@ -16,7 +16,7 @@
 				<li><a href="home.php">หน้าแรก</li>
 				<li><a href="room.php">ห้องพัก&ราคา</li>
 				<li><a herf="guest.php">สมุดเยี่ยมเยี่ยม</li>
-				<li><a href="contact.php">ติดต่อ</li>
+				<li><a href="contact.php">ติดต่อได้เลยจ้า</li>
 				</ul><p>
 				<?php include "login_form.php";?>
 			</td>
